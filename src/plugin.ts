e1ac94fabf508c6431e4abd18a915f1d14225ccd
Joël Charles <joel.charles91@gmail.com>
2020-04-07 14:35:20 +0200

    append requestContext to error handler params

--- src/plugin.ts
+++ src/plugin.ts
@@ -1,16 +1,20 @@
 import { ApolloServerPlugin, GraphQLRequestListener } from 'apollo-server-plugin-base';
+import { GraphQLRequestContext, WithRequired } from 'apollo-server-types';
 import { GraphQLError } from 'graphql';
 
 export class ApolloServerPluginError<T> implements ApolloServerPlugin<T> {
   constructor(
-    protected errorHandler: (e: GraphQLError) => any,
+    protected errorHandler: (e: GraphQLError, requestContext: WithRequired<
+      GraphQLRequestContext<T>,
+      'metrics' | 'source' | 'errors'
+      >) => any,
   ) {}
 
   requestDidStart(): GraphQLRequestListener<T> {
     return {
       didEncounterErrors: requestContext => {
         requestContext.errors.forEach(err => {
-          this.errorHandler(err);
+          this.errorHandler(err, requestContext);
         });
       },
     };
